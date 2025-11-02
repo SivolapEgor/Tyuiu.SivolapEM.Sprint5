c2aad5738585a4e8bfdff62162c782dd1f1613e7
sbuw <esivolap1@gmail.com>
2025-11-02 19:28:10 +0500

    upd2

--- Tyuiu.SivolapEM.Sprint5.Task3.V30.Lib/DataService.cs
+++ Tyuiu.SivolapEM.Sprint5.Task3.V30.Lib/DataService.cs
@@ -8,9 +8,11 @@ namespace Tyuiu.SivolapEM.Sprint5.Task3.V30.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
-            double z = (Math.Pow(x, 3) - 1) / 4 * Math.Pow(x, 2);
+            double numerator = Math.Pow(x, 3) - 1;
+            double denominator = 4 * Math.Pow(x, 2);
+            double z = numerator / denominator;
             z = Math.Round(z, 3);
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
             {
                 writer.Write(BitConverter.GetBytes(z));
             }
